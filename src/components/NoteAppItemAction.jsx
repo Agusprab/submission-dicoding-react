4c04fec14e8a4fe50848e0019139ad2b92a78545
Agus Prabowo <42889902+Agusprab@users.noreply.github.com>
2024-03-19 19:09:47 +0700

    Update NoteAppItemAction.jsx

--- src/components/NoteAppItemAction.jsx
+++ src/components/NoteAppItemAction.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function NoteAppItemAction({id,onDelete,onArchive,isArsip}){
     return(
         <div className="note-item__action">
-            <button className="note-item__delete-button" onClick={() => onDelete(id)}>Delete</button>
-            <button className="note-item__archive-button" onClick={() => onArchive(id)}>{isArsip === false ? 'Arsipkan' : 'Pindahkan'}</button>
+            <button className="note-item__delete-button" onClick={() => onDelete(id)}><i class="fa fa-trash" aria-hidden="true"></i></button>
+            <button className="note-item__archive-button" onClick={() => onArchive(id)}>{isArsip === false ? <i class="fa fa-archive" aria-hidden="true"></i>: <i class="fa fa-arrow-right" aria-hidden="true"></i>}</button>
         </div>
     )
 }
